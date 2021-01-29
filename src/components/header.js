6918fa05676ee155e608066e62c0079d8c0ed3ce
Shkim04 <gims86410524@gmail.com>
2021-01-29 23:34:51 +0900

    updated for responsive

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
 import './header.css';
 import BlogLogo from '../assets/site_logo.png';
+import ClickNavBar from './clickEventHandlerForNavBar';
 
 export default class Header extends Component {
-   handleDropDown = () => {
-      let dropDown = document.getElementById('menu-dropdown');
-      if(dropDown.className === 'top-nav-bar') {
-         dropDown.className += ' responsive';
+   constructor(props) {
+      super(props);
+      
+      this.state = {
+         menuOpen: false
       }
-      else {
-         dropDown.className = 'top-nav-bar';
-      }
+   }
+   
+   handleTopNavDropDown = () => {
+      this.setState(prevState => ({
+         menuOpen: !prevState.menuOpen
+      }))
    }
    render() {
       return (
@@ -20,15 +25,18 @@ export default class Header extends Component {
                <div className='site-name-container'>
                   <NavLink to='/'><img className='site-name' src={BlogLogo} alt='home' /></NavLink>
                </div>
-               <div className='top-nav-bar'  id='menu-dropdown' onClick={this.handleDropDown}>
+               <div className='top-nav-bar' onClick={this.handleTopNavDropDown}>
                   <div className='menu'>
                      <button className='menu-btn'>
                         <i className="fas fa-bars"></i>
                      </button>
-                     <div className='menu-content'>
-                        <NavLink to='/about_blog'>About Blog</NavLink>
-                        <a href='http://www.freecodecamp.org' alt='mentor-website'>freecodecamp.org</a>
-                     </div>
+                     {
+                        this.state.menuOpen &&
+                        <ClickNavBar className='menu-content' onClick={this.handleTopNavDropDown}>
+                           <NavLink to='/about_blog'>About Blog</NavLink>
+                           <a href='http://www.freecodecamp.org' alt='mentor-website'>freecodecamp.org</a>
+                        </ClickNavBar>
+                     }
                   </div>
                </div>
             </div>
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './navBar.css';
+import ClickNavBar from './clickEventHandlerForNavBar';
 
 export default class NavBar extends Component {
-    handleDropDown = (event) => {
-        const clicked = event.currentTarget;
-        const sideNav = clicked.parentNode.childNodes;
-        console.log(clicked);
-        
-        sideNav.forEach(el => {
-            if(el.classList.contains('bottom-nav-responsive')){
-                el.classList.remove('bottom-nav-responsive');
-            }
-        })
-        console.log(sideNav);
-        clicked.classList.toggle('bottom-nav-responsive')
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            infoOpen: false,
+            projectOpen: false,
+        }
+    }
+    componentDidMount() {
+        if(this.state.infoOpen) { document.getElementById('info').focus() };
+        if(this.state.projectOpen) { document.getElementById('project').focus() };
+    }
+    handleInfoNav = () => {
+        this.setState(prevState => ({
+            infoOpen: !prevState.infoOpen
+        }))
+    }
+
+    handleProjectNav = () => {
+        this.setState(prevState => ({
+            projectOpen: !prevState.projectOpen
+        }))
     }
     render() {    
         return (
@@ -28,49 +39,56 @@ export default class NavBar extends Component {
                             </NavLink>
                         </div>
                     </div>
-                    <div className='sidenav-nested-bar-container' onClick={this.handleDropDown}>
+                    <div className='sidenav-nested-bar-container' id='info' tabIndex='-1' onClick={this.handleInfoNav}>
                         <div className='nav-bar-title-container'>
                             <i className="fas fa-info-circle"></i>
                             <span className='nav-bar-title'>Info</span>
                         </div>
-                        <div className='sidenav-nested-bar clearfix'>
-                            <div className='nav-bar-list'>
-                                <NavLink to='/about_me' activeClassName='active'>
-                                    About Me
-                                </NavLink>
-                            </div>
-                            <div className='nav-bar-list'>
-                                <NavLink to='/goal' activeClassName='active'>
-                                    Goal
-                                </NavLink>
-                            </div>
-                        </div>
+                        {
+                            this.state.infoOpen &&
+                            <ClickNavBar className='sidenav-nested-bar clearfix' onClick={this.handleInfoNav}>
+                                <div className='nav-bar-list'>
+                                    <NavLink to='/about_me' activeClassName='active'>
+                                        About Me
+                                    </NavLink>
+                                </div>
+                                <div className='nav-bar-list'>
+                                    <NavLink to='/goal' activeClassName='active'>
+                                        Goal
+                                    </NavLink>
+                                </div>
+                            </ClickNavBar>
+                        }
                     </div>
-                    <div className='sidenav-nested-bar-container' onClick={this.handleDropDown}>
+                    <div className='sidenav-nested-bar-container' id='project' tabIndex='-1' onClick={this.handleProjectNav}>
                         <div className='nav-bar-title-container'>
                             <i className="fas fa-project-diagram"></i>
                             <span className='nav-bar-title'>Project</span>
                         </div>
-                        <div className='sidenav-nested-bar clearfix'>
-                            <div className='nav-bar-list'>
-                                <NavLink to='/beginning' activeClassName='active'>   
-                                    Begining
-                                </NavLink>
-                            </div>
-                            <div className='nav-bar-list'>
-                                <NavLink to='/trips_tracker' activeClassName='active'>
-                                    Trip Tracker
-                                </NavLink>
-                            </div>
-                            <div className='nav-bar-list'>
-                                <NavLink to='/addition_practice' activeClassName='active'>
-                                    Addition Practice
-                                </NavLink>
-                            </div>
-                        </div>
+                        {
+                            this.state.projectOpen &&
+                            <ClickNavBar className='sidenav-nested-bar clearfix' onClick={this.handleProjectNav}>
+                                <div className='nav-bar-list'>
+                                    <NavLink to='/beginning' activeClassName='active'>   
+                                        Beginning
+                                    </NavLink>
+                                </div>
+                                <div className='nav-bar-list'>
+                                    <NavLink to='/trips_tracker' activeClassName='active'>
+                                        Trip Tracker
+                                    </NavLink>
+                                </div>
+                                <div className='nav-bar-list'>
+                                    <NavLink to='/addition_practice' activeClassName='active'>
+                                        Addition Practice
+                                    </NavLink>
+                                </div>
+                            </ClickNavBar>
+                        }
                     </div>
                 </div>
             </div>
         )
     }
 }
+
